e49dfddee74fd52beffa2e1b9b2efcfac61cd8a7
OmegaLarmor <remy-lapointe@hotmail.ca>
2017-05-12 22:39:12 -0400

    Organize + camera switch
    Thought the amount of prefabs was getting a bit big, so made separate
    folders. Added the option to switch to an overhead camera by holding the
    space bar.

---
+++ Assets/CameraDisplay.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDisplay : MonoBehaviour {
+
+    public Camera playerCamera; //aka the main camera
+    public Camera overheadCamera; //duplicate of minimap camera with no Minimap Target Texture
+    public GameObject GUILayout; //The Canvas GameObject
+
+    private Canvas canvas; //The Canvas component
+
+    // Use this for initialization
+    void Start () {
+
+        playerCamera.GetComponent<Camera>().enabled = false;  //Start at default position
+        overheadCamera.GetComponent<Camera>().enabled = true;
+        canvas = GUILayout.GetComponent<Canvas>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            playerCamera.GetComponent<Camera>().enabled = false;
+            overheadCamera.GetComponent<Camera>().enabled = true;
+            canvas.worldCamera = overheadCamera;  //Moves the UI
+        }
+        else
+        {
+            playerCamera.GetComponent<Camera>().enabled = true;  //return to main camera
+            overheadCamera.GetComponent<Camera>().enabled = false;
+            canvas.worldCamera = playerCamera;
+        }
+		
+	}
+}
