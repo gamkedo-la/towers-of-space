80e051e79105526312be788de75907fd119f2e32
Kornel <Kornel@Kornel.com>
2017-06-18 14:35:08 +0200

    Graphical upgrades: new starting and end point, nodes color and glow changes, background change.

---
+++ Assets/Scripts/MeterialPulse.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class MeterialPulse : MonoBehaviour
+{
+	[SerializeField] private float duration = 1f;
+	[SerializeField] private float minValue = 0.8f;
+	[SerializeField] private float maxValue = 1f;
+
+	private Renderer rend;
+	private Material matModel;
+	private Color matColor;
+
+	void Awake ()
+	{
+		GetMaterial( );
+		matColor = matModel.GetColor( "_EmissionColor" );
+	}
+
+	private void GetMaterial( )
+	{
+		rend = GetComponent<Renderer>( );
+		Assert.IsNotNull( rend );
+
+		matModel = rend.material;
+		Assert.IsNotNull( matModel );
+	}
+
+	void Update( )
+	{
+		float lerp = Mathf.PingPong( Time.time, duration ) / duration;
+		matColor.g = minValue + ( ( maxValue - minValue ) * lerp );
+
+		matModel.SetColor( "_EmissionColor", matColor );
+	}
+}
