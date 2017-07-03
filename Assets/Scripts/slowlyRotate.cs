acbce3273c6f995ad24dc28a27f414c05a1925e8
Christer Kaitila <github@mcfunkypants.com>
2017-07-03 15:28:25 -0700

    logo / main menu

---
+++ Assets/Scripts/slowlyRotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slowlyRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			// Slowly rotate the object arond its X axis at 1 degree/second.
+			transform.Rotate(Time.deltaTime, 0, 0);		
+	}
+}
