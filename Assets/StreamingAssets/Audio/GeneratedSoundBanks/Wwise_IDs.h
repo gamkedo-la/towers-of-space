7fe89e52b705be1b68956b7c98c7dbe32145a58a
M D Fewkes <mdfewkes@gmail.com>
2017-07-10 22:34:56 -0600

    Added Wwise hooks for volumes and mutes
    Created RTPCs for volume control and mutes.

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,17 +14,24 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_AP_SHOT = 1856089824U;
+        static const AkUniqueID PLAY_ARMORTANK_HIT = 2636252289U;
         static const AkUniqueID PLAY_EMP_SHOT = 2111735307U;
         static const AkUniqueID PLAY_LASER_SHOT = 4185646314U;
         static const AkUniqueID PLAY_LEVEL_MUSIC = 557932600U;
         static const AkUniqueID PLAY_TANK_EXPLO = 531438269U;
+        static const AkUniqueID PLAY_TANK_FIRE = 1956447669U;
         static const AkUniqueID PLAY_TANK_HIT = 3748445276U;
         static const AkUniqueID PLAY_TOWER_BUILD = 2943239532U;
+        static const AkUniqueID PLAY_TOWER_EXPLO = 4289585650U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS
     {
         static const AkUniqueID LIFE = 2137943U;
+        static const AkUniqueID MUSIC_MUTE = 1696404602U;
+        static const AkUniqueID MUSIC_VOLUME = 1006694123U;
+        static const AkUniqueID SFX_MUTE = 2835551186U;
+        static const AkUniqueID SFX_VOLUME = 1564184899U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
