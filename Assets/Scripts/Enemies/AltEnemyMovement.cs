efe45ea1268ba86bee0ae562ff0952f6d58b814b
sergiosolorzano <sergiosolorzano@gmail.com>
2017-07-17 12:59:38 +0100

    scen4wip

---
+++ Assets/Scripts/Enemies/AltEnemyMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AltEnemyMovement : MonoBehaviour
+{
+
+    public Transform destination;
+
+    private UnityEngine.AI.NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        agent.SetDestination(destination.position);
+    }
+
+}
