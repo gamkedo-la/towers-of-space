5732095da809cafd807fb0f69a6c59431d0ec382
sergiosolorzano <sergiosolorzano@gmail.com>
2017-07-18 13:41:58 +0100

    scen4wip

--- Assets/Scripts/Enemies/AltEnemyMovement.cs
+++ Assets/Scripts/Enemies/AltEnemyMovement.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class AltEnemyMovement : MonoBehaviour
 {
 
-    public Transform destination;
+    //public Transform destination;
 
     private UnityEngine.AI.NavMeshAgent agent;
 
     void Start()
     {
+        Vector3 myVector = new Vector3 (6.4f, -4.52f, 13.48f);
+        
+        //Transform destination = transform.position(6.4, -4.52, 13.48);
         agent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
 
-        agent.SetDestination(destination.position);
+        //agent.SetDestination(destination.position);
+        agent.SetDestination(myVector);
     }
 
 }
