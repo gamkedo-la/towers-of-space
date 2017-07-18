6c6327679d58737db6075353c6304d2356e3fe34
sergiosolorzano <sergiosolorzano@gmail.com>
2017-07-18 17:53:30 +0100

    scen4wip

--- Assets/Scripts/Enemies/AltEnemyMovement.cs
+++ Assets/Scripts/Enemies/AltEnemyMovement.cs
@@ -11,13 +11,37 @@ public class AltEnemyMovement : MonoBehaviour
 
     void Start()
     {
-        Vector3 myVector = new Vector3 (6.4f, -4.52f, 13.48f);
-        
+        Vector3 myVector = new Vector3(6.4f, -4.52f, 13.48f);
+
         //Transform destination = transform.position(6.4, -4.52, 13.48);
         agent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
 
         //agent.SetDestination(destination.position);
         agent.SetDestination(myVector);
+
     }
+    //void Update()
+    //    {
+    //        if (!hasNextWaypoint)
+    //        {
+    //            SetNextNode();
+    //
+    //            if (!hasNextWaypoint)
+    //            {
+    //                // No more path nodes!
+    //                ReachedGoal();
+    //            }
+    //        }
+
+
+
+
+            //if (agent.transform.position = myVector)
+            //{
+            //    Destroy(gameObject);
+            //    GameController.instance.LoseLife(); //instance is the Score Manager
+            //    GameController.instance.CheckGameWon();
+            //}
+      //  }
 
 }
